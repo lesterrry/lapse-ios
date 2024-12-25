ad11b03e0232d38fa15b414f25be9456af32ca23
Yana Glukhova <yana.glukhoba@gmail.com>
2024-12-25 13:20:14 +1200

    Initial Commit

---
+++ Project_26_12_prosmotr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project_26_12_prosmotr
+//
+//  Created by MacBook - Latter on 25.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project_26_12_prosmotr/Project_26_12_prosmotrApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_26_12_prosmotrApp.swift
+//  Project_26_12_prosmotr
+//
+//  Created by MacBook - Latter on 25.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Project_26_12_prosmotrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
